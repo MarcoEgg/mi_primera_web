f94f09bc83586927757251236c7830b5ffa654ea
MarcoEgg <163218040+MarcoEgg@users.noreply.github.com>
2024-03-12 20:15:46 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// alert("Hola este es mi Javascript");
+
+//const nombre = "Marco";
+
+//console.log(nombre);
+
+let contenidoTitulo = "Acerca";
+
+let titulo = document.querySelector(".seccion-1 .b");
+console.log(titulo);
+
+//titulo.innerHTML = contenidoTitulo;
+
+// CONDICIONALES
+
+//let textoTitulo = titulo.innerText;
+//console.log(textoTitulo);
+
+
+//if (textoTitulo == "Acerca"){
+//    titulo.innerHTML = "Otro";
+//} else { 
+//   console.log("No se cumple")
+// }
